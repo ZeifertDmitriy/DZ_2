d0974d809178fac18e03446f24eda29588e4ccc8
Dmitriy <zeifd@mail.ru>
2022-04-21 01:29:12 +0700

    Homework

---
+++ dz_2/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 19. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+Console.WriteLine("Введите пятизначное число: ");
+int number = int.Parse(Console.ReadLine());
+if (Convert.ToString(number).Length == 5)
+{
+    if ((number % 10 == number / 10000) & ((number % 100) / 10 == (number / 1000) % 10))
+    {
+        Console.WriteLine("Число является палиндромом.");
+    }
+    else
+    {
+        Console.WriteLine("Число не является палиндромом.");
+    }
+}
+else
+{
+    Console.WriteLine("Введено не пятизначное число.");
+}
+
+Console.WriteLine("------------------------------------------");
+// Задача 21. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+int x1 = new Random().Next(1, 10);
+int y1 = new Random().Next(1, 10);
+int z1 = new Random().Next(1, 10);
+int x2 = new Random().Next(1, 10);
+int y2 = new Random().Next(1, 10);
+int z2 = new Random().Next(1, 10);
+//x1 = 3;
+//y1 = 6;
+//z1 = 8;
+//x2 = 2;
+//y2 = 1;
+//z2 = -7;
+double result = Math.Sqrt(Math.Pow((x1 - x2), 2) + Math.Pow((y1 - y2), 2) + Math.Pow((z1 - z2), 2));
+Console.WriteLine(result);
+
+Console.WriteLine("------------------------------------------");
+// Задача 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int index = 1;
+while (index < num)
+{
+    Console.Write(Math.Pow(index, 3) + ", ");
+    index++;
+}
+Console.Write(Math.Pow(index, 3) + ".");
---
+++ test_4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое число: ");
+int num = int.Parse(Console.ReadLine());
+int num1 = 7;
+int num2 = 23;
+if ((0 == num % num1) | (0 == num% num2))
+{
+    Console.WriteLine("кратно");
+}
+else
+{
+    Console.WriteLine("не кратно");
+}
